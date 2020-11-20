66abad7c844382f80f11d3c0417497bd98cff2a1
Nator Verinumbe <natverior1@gmail.com>
2020-11-20 10:42:40 +0100

    Assigned methods to specific for handlers

--- main.go
+++ main.go
@@ -16,13 +16,14 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./"))
 
 	fmt.Println("server running...@localhost:9080")
-	myRouter.HandleFunc("/index", views.Index)
+	myRouter.HandleFunc("/index", views.Index).Methods("GET")
+	myRouter.HandleFunc("/insert", views.Insert).Methods("GET")
 	myRouter.Handle("/css/bootstrap.min.css", fileServer)
 	myRouter.Handle("/js/bootstrap.min.js", fileServer)
 
-	myRouter.HandleFunc("/api/fetch", controllers.Fetch)
-	myRouter.HandleFunc("/api/update", controllers.Update)
-	myRouter.HandleFunc("/api/delete", controllers.Delete)
+	myRouter.HandleFunc("/api/fetch", controllers.Fetch).Methods("GET")
+	myRouter.HandleFunc("/api/update", controllers.Update).Methods("POST")
+	myRouter.HandleFunc("/api/delete", controllers.Delete).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":9080", myRouter))
 
 }
