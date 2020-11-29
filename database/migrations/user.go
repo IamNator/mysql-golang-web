4cd1cc65ef2bb6fbfcf1e9e95027a8fc8035adc3
Nator Verinumbe <natverior1@gmail.com>
2020-11-29 10:22:44 +0100

    adding database migrations, engine and seeders

---
+++ database/migrations/user.go
@@ -0,0 +1,13 @@
+package migrations
+//
+//import "database/sql"
+//
+//func CreateUserDb(){
+//	sql.E
+//}
+
+//CREATE TABLE users(
+//id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//username VARCHAR(50),
+//password VARCHAR(120)
+//);
