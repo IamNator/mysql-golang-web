479daa142b161e652244b57b067f9d1d8f01ac72
Nator Verinumbe <natverior1@gmail.com>
2020-12-14 13:22:06 +0100

    debuggin login

--- user/login.go
+++ user/login.go
@@ -12,22 +12,23 @@ import (
 
 func (db *DBData) Login(w http.ResponseWriter, req *http.Request) {
 	var user LoginUser
-	var userdb RegisterUser
+	var userDb RegisterUser
 	var id string
 	err := json.NewDecoder(req.Body).Decode(&user)
 	check(err)
 
 	fmt.Println(user.UserName)
 
-	err = db.Session.QueryRow("SELECT id, username, password FROM users WHERE username=?", user.UserName).Scan(&id, &userdb.UserName, &userdb.Password)
+	err = db.Session.QueryRow("SELECT id, username, password FROM users WHERE username=?", user.UserName).Scan(&id, &userDb.UserName, &userDb.Password)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("dbQuery Error %v \n", err)
 		http.Error(w, "User not found", http.StatusNotFound)
 		return
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(userdb.Password), []byte(user.Password))
+	err = bcrypt.CompareHashAndPassword([]byte(userDb.Password), []byte(user.Password))
 	if err != nil {
+		fmt.Printf("CompareHashPassword Error %v \n", err)
 		http.Error(w, "Password incorrect", http.StatusNotFound)
 		return
 	}
