99e5fc5f808026a225f0f48a4ebaa9a584bedb6f
Nator Verinumbe <natverior1@gmail.com>
2020-12-14 14:21:00 +0100

    Changed error message to json in Register Api

--- user/register.go
+++ user/register.go
@@ -18,14 +18,14 @@ func (db *DBData) Register(w http.ResponseWriter, req *http.Request) {
 	case err == sql.ErrNoRows:
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 		if err != nil {
-			http.Error(w, "server Error, unable to create your account (hash problem)", 500)
+			JsonRegisterError(&w,"server Error, unable to create your account (hash problem)", http.StatusInternalServerError)
 			log.Fatal(err)
 			return
 		}
 
 		_, err = db.Session.Exec("INSERT INTO users(username, password) VALUES(?,?)", user.UserName, hashedPassword)
 		if err != nil {
-			http.Error(w, "Server error, unable to create your account (db problem)", 500)
+			JsonRegisterError(&w,"server Error, unable to create your account (Database problem)", http.StatusInternalServerError)
 			log.Fatal(err)
 			return
 		}
@@ -33,7 +33,7 @@ func (db *DBData) Register(w http.ResponseWriter, req *http.Request) {
 		w.Write([]byte("User Created!"))
 		return
 	case err != nil:
-		http.Error(w, "server error, Unable to create your account", 500)
+		JsonRegisterError(&w,"server error, Unable to create your account",http.StatusInternalServerError)
 		fmt.Print(err)
 		return
 	default:
@@ -41,3 +41,13 @@ func (db *DBData) Register(w http.ResponseWriter, req *http.Request) {
 	}
 
 }
+
+func JsonRegisterError(w * http.ResponseWriter, ErrorMessage string, ErrorCode int) {
+	(*w).WriteHeader(ErrorCode)
+	json.NewEncoder(*w).Encode(struct{
+		Login string `json:"login"`
+	}{
+		ErrorMessage,
+	})
+}
+
