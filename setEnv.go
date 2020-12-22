2ceca636817fadffe6142c19888ef541bdf1e12d
Nator Verinumbe <natverior1@gmail.com>
2020-12-22 18:04:08 +0100

    updating setEnv

--- setEnv.go
+++ setEnv.go
@@ -6,14 +6,15 @@ import (
 )
 
 func main(){
-	fmt.Println(os.UserHomeDir())
 	as := os.Getenv("ACCESS_SECRET")
-	fmt.Println("ACCESS_SECRET Already set")
-	if as == "" {
+	//fmt.Println(string(as))
+	if as != "Our-web-app" {
 		err := os.Setenv("ACCESS_SECRET", "Our-web-app")
 		if err != nil {
 			fmt.Println("failed to set ACCESS_SECRET")
 		}
 		fmt.Println("ACCESS_SECRET Set!")
+	} else {
+		fmt.Println("ACCESS_SECRET Already set")
 	}
 }
