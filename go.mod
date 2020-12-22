6599c88a5ecf7e40c82e0c06a6ef30fc0cc6b62d
Nator Verinumbe <natverior1@gmail.com>
2020-12-22 17:01:13 +0100

    more updates to createToken func

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	cloud.google.com/go v0.74.0 // indirect
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
 	github.com/satori/go.uuid v1.2.0
--- session/jwt.go
+++ session/jwt.go
@@ -1,9 +1,14 @@
 package session
 
-import "os"
+import (
+	"github.com/dgrijalva/jwt-go"
+	"os"
+)
 
 func CreateToken(userid string) (token string, err error){
 	err = os.Setenv("ACCESS_SECRET", "Our-web-app")
+	atClaim := jwt.MapClaims{}
+	atClaim["authorized"] = true
 
 	return token, err
 }
