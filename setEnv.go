80e752f48ff7b4f567c783b2c5a782191ee88337
Nator Verinumbe <natverior1@gmail.com>
2020-12-22 17:49:56 +0100

    edited .env file

--- setEnv.go
+++ setEnv.go
@@ -7,10 +7,12 @@ import (
 
 func main(){
 	as := os.Getenv("ACCESS_SECRET")
+	fmt.Println("ACCESS_SECRET Already set")
 	if as == "" {
 		err := os.Setenv("ACCESS_SECRET", "Our-web-app")
 		if err != nil {
 			fmt.Println("failed to set ACCESS_SECRET")
 		}
+		fmt.Println("ACCESS_SECRET Set!")
 	}
 }
