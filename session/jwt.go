9b12f64b28cf0c9c84348570ccae04772789c0d1
Nator Verinumbe <natverior1@gmail.com>
2020-12-22 17:08:27 +0100

    finished createToken func

--- session/jwt.go
+++ session/jwt.go
@@ -3,12 +3,20 @@ package session
 import (
 	"github.com/dgrijalva/jwt-go"
 	"os"
+	"time"
 )
 
 func CreateToken(userid string) (token string, err error){
-	err = os.Setenv("ACCESS_SECRET", "Our-web-app")
 	atClaim := jwt.MapClaims{}
 	atClaim["authorized"] = true
+	atClaim["user_id"] = userid
+	atClaim["exp"] = time.Now().Add(time.Minute * 30).Unix()
 
-	return token, err
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaim)
+	token, err = at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
 }
