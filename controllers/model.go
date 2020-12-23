4499778030758bc439b3606d70188da3cbe1c291
Nator Verinumbe <natverior1@gmail.com>
2020-12-23 17:13:47 +0100

    adding a validation to data

--- controllers/model.go
+++ controllers/model.go
@@ -3,11 +3,7 @@ package controllers
 import "database/sql"
 
 type DBData struct {
-	DBType string
-	User string
-	Password string
-	Host	string
-	DBName	string
+	DBType, User, Password, Host, DBName string
 	Session                              *sql.DB
 	SessionIDs                           map[string]string
 	SessionToken                         map[string]string
