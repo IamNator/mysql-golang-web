aeeb4f4a85e7e5bc09d2fb77a5b555c3a8fe1528
Nator Verinumbe <natverior1@gmail.com>
2020-12-24 15:01:36 +0100

    testing on local machine

--- main.go
+++ main.go
@@ -25,25 +25,25 @@ import (
 */
 
 func main() {
-	//dbGeneral := models.DBData{
-	//	DBType:       "mysql",                       //Type
-	//	User:         "b7e0a0a81fef1f",              //User
-	//	Password:     "2e02951d",                    //Password
-	//	Host:         "eu-cdbr-west-03.cleardb.net", //Host 3306
-	//	DBName:       "heroku_31043c4e11d34ce",      //DBName
-	//	Session:      nil,                           //Session for db
-	//	SessionToken: make(map[string]models.UserCredentials),    // map[string]struct [token]userDetails
-	//}
-
 	dbGeneral := models.DBData{
-		DBType:   "mysql",          //Type
-		User:     "root",    	    //User
-		Password: "299792458m/s",   //Password
-		Host:     "localhost:3306", //Host 3306
-		DBName:   "app",  			//DBName
-		Session:  nil,              //Session
-		SessionToken: make(map[string]models.UserCredentials),	// map[string]string
+		DBType:       "mysql",                       //Type
+		User:         "b7e0a0a81fef1f",              //User
+		Password:     "2e02951d",                    //Password
+		Host:         "eu-cdbr-west-03.cleardb.net", //Host 3306
+		DBName:       "heroku_31043c4e11d34ce",      //DBName
+		Session:      nil,                           //Session for db
+		SessionToken: make(map[string]models.UserCredentials),    // map[string]struct [token]userDetails
 	}
+
+	//dbGeneral := models.DBData{
+	//	DBType:   "mysql",          //Type
+	//	User:     "root",    	    //User
+	//	Password: "299792458m/s",   //Password
+	//	Host:     "localhost:3306", //Host 3306
+	//	DBName:   "app",  			//DBName
+	//	Session:  nil,              //Session
+	//	SessionToken: make(map[string]models.UserCredentials),	// map[string]string
+	//}
 	DB := controllers.Controllersdb(dbGeneral)
 	db, _ := DB.OpenDB()
 	DB.Session = db
