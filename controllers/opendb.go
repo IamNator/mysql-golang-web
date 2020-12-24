5edc8ac444e92f9af0ece1fba1dd01df5c0fb8c3
Nator Verinumbe <natverior1@gmail.com>
2020-12-24 14:52:21 +0100

    testing on local machine

--- controllers/opendb.go
+++ controllers/opendb.go
@@ -3,13 +3,12 @@ package controllers
 import (
 	"database/sql"
 	"fmt"
-	"time"
 )
 
 func (db Controllersdb) OpenDB() (*sql.DB, error) {
 	openDB, err := sql.Open(db.DBType, fmt.Sprintf("%s:%s@tcp(%s)/%s", db.User, db.Password, db.Host, db.DBName))
 	//db.Session.SetMaxOpenConns(20)
 	//db.Session.SetMaxIdleConns(20)
-	db.Session.SetConnMaxLifetime(time.Minute * 10)
+	//db.Session.SetConnMaxLifetime(time.Minute * 10)
 	return openDB, err
 }
