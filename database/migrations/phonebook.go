46be3b60f722d99d46218da0c6b06486da163119
Nator Verinumbe <natverior1@gmail.com>
2020-12-25 08:32:55 +0100

    fixing bugs

--- database/migrations/phonebook.go
+++ database/migrations/phonebook.go
@@ -1,14 +1,20 @@
 package migrations
 
+import (
+	"context"
+	"time"
+)
+
 func (db *Migrationdb) CreatePhoneBookDb() {
 
 	//query
 	_ = `CREATE TABLE IF NOT EXISTS phoneBook(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, userID INT NOT NULL, FirstName NCHAR(50),  
         LastName NCHAR(50), phoneNumber  VARCHAR(16), created_at datetime default CURRENT_TIMESTAMP, updated_at datetime default CURRENT_TIMESTAMP)`
 
-	//ctx, cancelfunc := context.WithTimeout(context.Background(), 15*time.Second)
-	//defer cancelfunc()
-	//
+	//ctx,
+	_, cancelfunc := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancelfunc()
+
 	//res, err := db.Session.ExecContext(ctx, query)
 	//if err != nil {
 	//	log.Printf("Error %s when creating product table", err)
