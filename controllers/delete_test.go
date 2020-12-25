45d746d347ca917de40159aa5cdde14e9f24031c
Nator Verinumbe <natverior1@gmail.com>
2020-12-25 08:03:53 +0100

    removed test function in delete_test file

--- controllers/delete_test.go
+++ controllers/delete_test.go
@@ -1,32 +1,32 @@
 package controllers_test
-
-import (
-	"github.com/IamNator/mysql-golang-web/controllers"
-	"net/http"
-	"net/http/httptest"
-	"testing"
-)
-
-var dbs controllers.DBData
-
-//Still need a ton of improvement
-func TestDBData_Delete(t *testing.T) {
-	req, err := http.NewRequest("POST", "/api/delete", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rr := httptest.NewRecorder()
-
-	handler := http.HandlerFunc(dbs.Delete)
-	handler.ServeHTTP(rr, req)
-
-	if rr.Code != http.StatusOK {
-		t.Errorf("handler returned unexpected status code : got %v wanted %v", rr.Code, http.StatusOK)
-	}
-
-	expected := "deleted"
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body : got %v wanted %v", rr.Body.String(), expected)
-	}
-
-}
+//
+//import (
+//	"github.com/IamNator/mysql-golang-web/controllers"
+//	"net/http"
+//	"net/http/httptest"
+//	"testing"
+//)
+//
+//var dbs controllers.DBData
+//
+////Still need a ton of improvement
+//func TestDBData_Delete(t *testing.T) {
+//	req, err := http.NewRequest("POST", "/api/delete", nil)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	rr := httptest.NewRecorder()
+//
+//	handler := http.HandlerFunc(dbs.Delete)
+//	handler.ServeHTTP(rr, req)
+//
+//	if rr.Code != http.StatusOK {
+//		t.Errorf("handler returned unexpected status code : got %v wanted %v", rr.Code, http.StatusOK)
+//	}
+//
+//	expected := "deleted"
+//	if rr.Body.String() != expected {
+//		t.Errorf("handler returned unexpected body : got %v wanted %v", rr.Body.String(), expected)
+//	}
+//
+//}
