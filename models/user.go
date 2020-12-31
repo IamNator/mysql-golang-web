bf8c26cff7f6daafe51998b6d65799c2bd47fbe6
Nator Verinumbe <natverior1@gmail.com>
2020-12-31 21:40:07 +0100

    a few changes

--- models/user.go
+++ models/user.go
@@ -11,7 +11,7 @@ type PhoneBookContact struct {
 	ID          string `json:"id"`
 }
 
-//For database connec
+//For database connecti
 type DBData struct {
 	DBType string   `mapstructure:"DB_TYPE"`
 	User string     `mapstructure:"USER"`
