f0dad5a5093a0f988f94257cf56c549914295d0a
Nator Verinumbe <natverior1@gmail.com>
2020-12-31 21:53:52 +0100

    a few changes --  more documentation

--- main.go
+++ main.go
@@ -74,7 +74,7 @@ func main() {
 
 	dbUser := user.Sessiondb(DB) //session
 
-	// checks if DB table
+	// checks if DB table exi
 	if !DB.DbExists() {
 		CreateAndFillDb(DB)
 		fmt.Println("Database created and updated")
