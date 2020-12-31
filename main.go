a991486fe8646be8ec35b047e7ad86cb23a9d2d6
Nator Verinumbe <natverior1@gmail.com>
2020-12-31 21:46:25 +0100

    a few changes

--- main.go
+++ main.go
@@ -127,7 +127,7 @@ func CreateAndFillDb(db controllers.Controllersdb) {
 	dbSeeders.FillDb()
 }
 
-//extracts important information from app.env
+//extracts important information from app.env file
 func LoadConfig(path string) (config models.DBData, err error) {
 
 	viper.AddConfigPath(path)
