30f282ab2a8aa0bc3a8e3c0d5e8033f802300b10
Nator Verinumbe <natverior1@gmail.com>
2020-12-31 21:43:33 +0100

    a few changes

--- session/token.go
+++ session/token.go
@@ -8,7 +8,7 @@ import (
 
 var Mutex sync.Mutex
 
-//creates a token for a user { we should store user
+//creates a token for a user { we should store user tokens
 func CreateToken(db *Sessiondb, user models.UserCredentials) (token string) {
 	token = uuid.NewV1().String()
 	Mutex.Lock()
