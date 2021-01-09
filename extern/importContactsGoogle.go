78c31d5b580b23635af923e35039d6301ff58436
Nator Verinumbe <natverior1@gmail.com>
2021-01-09 17:36:42 +0100

    adding aget function to retrieve contacts from google cloud

--- extern/importContactsGoogle.go
+++ extern/importContactsGoogle.go
@@ -8,49 +8,33 @@ import (
 	"context"
 	"fmt"
 	"github.com/IamNator/mysql-golang-web/models"
-	"io"
-	"log"
 	"net/http"
 	"time"
 )
 
 type db models.DBData
 
-
-
 func (db *db) getContact(){
+
 	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Duration(5 * time.Second))
 	defer cancelFunc()
 
-	respBody, err := fetchContact(ctx)
+	req, err := http.NewRequestWithContext(
+		ctx,
+		"GET",
+		"https://people.googleapis.com/v1/%5BRESOURCENAME%5D/connections?key=[YOUR_API_KEY] HTTP/1.1",
+		nil,
+		)
+
+	client := http.Client{}
+	req.Header.Set("Authorization", "Bearer [YOUR_ACCESS_TOKEN]")
+	req.Header.Set("Accept", "application/json")
+	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("request err : ", err.Error())
+		fmt.Println(" Error : ", err )
+		return
 	}
 
-	fmt.Println(respBody)
+	fmt.Println(resp.Body)
 }
 
-
-func fetchContact(ctx context.Context) (* io.ReadCloser, error) {
-
-	rwChan := make(chan io.ReadCloser)
-	errContextTimedOut := fmt.Errorf("context timed out")
-
-	go func() {
-		resp, err := http.Get("https://people.googleapis.com/v1/%5BRESOURCENAME%5D/connections?key=[YOUR_API_KEY] HTTP/1.1")
-		//req, err := http.NewRequestWithContext()
-		if err != nil {
-			log.Println("request err : ", err.Error())
-		}
-		rwChan <- resp.Body
-	}()
-
-	for {
-		select {
-			case rw := <- rwChan :
-				return &rw, nil
-			case <- ctx.Done() :
-				return nil, errContextTimedOut
-		}
-	}
-}
